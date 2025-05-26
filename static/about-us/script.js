5e1357be385ae499441249c216296978516fa4de
• Shasankbt • <shasankbt@Shasankbts-Mac-mini.local>
2025-05-26 22:21:46 +0530

    split the main index.html into files, working on navigation

--- static/about-us/script.js
+++ static/about-us/script.js
@@ -73,11 +73,12 @@ for animating the navbar
   //Menu On Hover
 
   $("body").on("mouseenter mouseleave", ".nav-item", function (e) {
+    console.log("mouse enter");
     if ($(window).width() > 750) {
       var _d = $(e.target).closest(".nav-item");
-      _d.addClass("show");
+      // _d.addClass("show");
       setTimeout(function () {
-        _d[_d.is(":hover") ? "addClass" : "removeClass"]("show");
+        // _d[_d.is(":hover") ? "addClass" : "removeClass"]("show");
       }, 1);
     }
   });
