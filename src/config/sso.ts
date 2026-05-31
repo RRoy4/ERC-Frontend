2b3a596b918f3df8416fe777bdefbc8c1efbdb5a
Sachin Mandal <sachinmandal3580@gmail.com>
2026-05-31 19:47:40 +0530

    Comment out PROJECT_ID export in sso.ts
    Comment out the PROJECT_ID export in sso.ts

--- src/config/sso.ts
+++ src/config/sso.ts
@@ -1,3 +1,3 @@
-export const PROJECT_ID = import.meta.env.VITE_SSO_PROJECT_ID
+// export const PROJECT_ID = import.meta.env.VITE_SSO_PROJECT_ID
 export const SSO_URL = `https://sso.tech-iitb.org/project/780c4ab-afcd-4b17-ab10-caf8e3abd0d7/ssocall/`
 export const SSO_USERDATA_URL = 'https://sso.tech-iitb.org/project/getuserdata'
